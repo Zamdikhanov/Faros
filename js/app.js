7c3e00a8057adb2a613af87e11946ca333ab776e
zamdikhanov <oxothuk.sky@mail.ru>
2021-11-04 22:40:31 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,120 @@
+wrapSize();
+
+function wrapSize() {
+    let wrap = document.querySelectorAll('.wrap--center');
+    let wrapSelectors = document.querySelectorAll('input[name="wrap-selector"]');
+
+    for (let wrapSelector of wrapSelectors) {
+        wrapSelector.addEventListener("change", () => {
+            if (wrapSelector.value === "center") {
+                for (let wr of wrap) { wr.classList.remove('wrap--full') };
+            }
+            if (wrapSelector.value === "full") {
+                for (let wr of wrap) { wr.classList.add('wrap--full') };
+            }
+        })
+    }
+}
+
+window.onscroll = function() { menuSticky() };
+
+var nav = document.querySelector(".nav");
+var sticky = nav.offsetTop;
+
+function menuSticky() {
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("nav--sticky")
+    } else {
+        nav.classList.remove("nav--sticky");
+    }
+    // console.log("scroll", window.pageYOffset);
+    // console.log("sticky", sticky);
+}
+
+const cards = document.querySelectorAll('.luminaire-card');
+cards.forEach(card => card.addEventListener('mouseenter', (() => sliderHover(card))));
+
+function sliderHover(card) {
+    slider = card.querySelector('.slider-hover');
+    if (slider.classList.contains("slider-hover--activated")) return 1;
+    slider.classList.add("slider-hover--activated");
+    const imgAll = slider.querySelectorAll("img");
+    const hoverArea = document.createElement('div');
+    hoverArea.classList.add("slider-hover__area");
+    slider.append(hoverArea);
+    for (let i = 0, length = imgAll.length; i < length; i++) {
+        hoverArea.insertAdjacentHTML("beforeEnd", `<div class="slider-hover__box"></div>`);
+    }
+    const hoverBoxes = hoverArea.querySelectorAll('.slider-hover__box');
+    hoverBoxes.forEach((hoverBox, index) => hoverBox.addEventListener('mouseenter', (() => sliderImgActivate(index, imgAll))));
+}
+
+function sliderImgActivate(count, imgArray) {
+    imgArray.forEach(img => img.classList.remove('slider-hover__img--active'));
+    imgArray[count].classList.add('slider-hover__img--active');
+}
+
+
+const filterGroup = document.querySelectorAll('.filter__btn-group');
+let arraySelectedFilters = [];
+for (let i = 0; i < filterGroup.length; i++) {
+    arraySelectedFilters.push('');
+}
+
+let filtersButtons = [];
+filterGroup.forEach((group, index) => {
+    filtersButtons[index] = group.querySelectorAll('.filter__btn')
+});
+
+for (let i = 0; i < arraySelectedFilters.length; i++) {
+    filtersButtons[i].forEach(button => {
+        button.addEventListener('click', (() => clickbutton(i, button)))
+    });
+}
+
+function clickbutton(filterIndex, button) {
+    if (button.dataset.filter == "all") {
+        arraySelectedFilters[filterIndex] = "";
+    } else {
+        arraySelectedFilters[filterIndex] = button.dataset.filter;
+    }
+    filtersButtons[filterIndex].forEach(button => button.classList.remove('active'));
+    button.classList.add('active');
+    console.log(arraySelectedFilters[filterIndex]); //////
+    showSelection();
+}
+
+
+function showSelection() {
+    cards.forEach(card => card.classList.add('luminaire-card--hide'));
+    cards.forEach(card => {
+        let matches = 0;
+        for (let i = 0; i < arraySelectedFilters.length; i++) {
+            if (card.dataset.properties.includes(arraySelectedFilters[i])) {
+                matches++;
+            } else continue;
+        }
+        if (matches === arraySelectedFilters.length) {
+            card.classList.remove('luminaire-card--hide');
+        }
+    });
+
+}
+
+
+function test(text) {
+    console.log(text);
+}
+
+
+const buttonsDetails = document.querySelectorAll(".b-details");
+buttonsDetails.forEach(buttonDetails => {
+    buttonDetails.addEventListener('click', (event) => showDetails(event));
+});
+
+function showDetails(event) {
+    event.target.classList.toggle("active");
+    const card = event.target.closest('.luminaire-card__inner');
+    card.querySelector('.luminaire-card__specifications').classList.toggle("details--hide");
+    setTimeout(() => card.classList.toggle("luminaire-card__inner--full"), 0);
+}
